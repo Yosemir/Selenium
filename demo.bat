eab8d7536f166767f611843e45efb446083b23e1
unknown <Yosemir>
2020-02-12 12:40:24 -0500

    Pruebas Selenium

--- demo.bat
+++
@@ -1,1 +0,0 @@
-start selenium-side-runner Asistencia.side
---
+++ src/main/java/com/vallegrande/seleniumvg/user.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vallegrande.seleniumvg;
+
+/**
+ *
+ * @author Root-Y
+ */
+public class user {
+    
+}
